3dcf4f425ff6e4c20aad38aa92b8574601051ad5
JonnyCBB <jonnybrooks04@gmail.com>
2017-01-15 19:02:09 +0000

    Add documentation

--- LongestHeadRun.py
+++ LongestHeadRun.py
@@ -1,7 +1,34 @@
+"""
+This is a script that contains functions which calculate properties associated
+with the longest run of heads in a coin toss experiment. These functions are
+implementations of the methods described in:
+Schilling, Mark F. "The longest run of heads." College Math. J 21.3 (1990):
+196-207.
+"""
 import numpy as np
 
 
 def A(n, x):
+    """
+    Calculate the number of sequences of length n in which the longest run of
+    heads does not exceed x.
+
+    Parameters
+    ----------
+    n : int
+        sequence length
+    x : int
+        longest run of heads
+
+    Returns
+    -------
+    num_seq : int
+        number of sequences
+
+    Examples
+    --------
+    >>> number_of_sequences = A(8, 3)
+    """
     num_seq = 0
     if n <= x:
         num_seq += 2**n
